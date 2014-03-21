0238c0264f0493f5d27627a56f8588a72ae2808b
timrighettini <tim.righettini@gmail.com>
2014-03-21 16:52:31 -0700

    Added in some packages and a test lighting scene.

---
+++ Prototype_v0/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+
+    #region public variables
+
+    GameObject m_lightPlayer;
+    GameObject m_ShadowPlayer;
+    GameObject m_TwilightPlayer;
+
+    #endregion
+
+    // Use this for initialization
+	void Start () {
+	    // Grab all of the player references, make it automated
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    // Do Input Polling
+
+	}
+}
