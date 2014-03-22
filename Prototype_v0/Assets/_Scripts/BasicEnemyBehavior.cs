d397be409ed7c042e86633ef271ca20c09d3c029
timrighettini <tim.righettini@gmail.com>
2014-03-22 15:24:59 -0700

    Got basic enemy follower in.  Logic has not been set for health yet.
    Enemy only follows shadow character

--- Prototype_v0/Assets/_Scripts/BasicEnemyBehavior.cs
+++ Prototype_v0/Assets/_Scripts/BasicEnemyBehavior.cs
@@ -3,19 +3,85 @@ using System.Collections;
 
 public class BasicEnemyBehavior : MonoBehaviour {
 
-    GameObject target;
+    #region public variables
 
-	// Use this for initialization
-	void Start () {
-	    
-	}
+    public GameObject m_Target;    
+    public float m_SpeedPerSecond;
+    public string m_TagOfTarget;
+
+    #endregion
+
+
+
+    #region private variables
+
+    private Vector3 m_VectorToTarget = new Vector3();
+    private Transform m_ThisTransform;
+
+    #endregion
+
+
+
+    #region unity overrides
+
+    // Use this for initialization
+    void Start()
+    {
+        m_ThisTransform = this.transform;
+
+        m_Target = GameObject.FindGameObjectWithTag(m_TagOfTarget);
+        
+        if (m_Target)
+        {
+            m_VectorToTarget = m_Target.transform.position - m_ThisTransform.position;
+        }
+        else
+        {
+            m_VectorToTarget = Vector3.zero;
+        }        
+    }
 
     //-------------------------------------------------------------------------
-	
-	// Update is called once per frame
-	void Update () {
-	    
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move in the direction towards the target, then update that direction
+        if (m_Target)
+        {
+            m_VectorToTarget = m_Target.transform.position - m_ThisTransform.position;            
+        }
+        else
+        {
+            m_VectorToTarget = Vector3.left;            
+        }        
+
+        // For basic enemy, we will only follow on the ground, so let's
+        // get rid of any y-movement
+
+        Vector3 forwardNoY = Vector3.Normalize(m_VectorToTarget);
+        forwardNoY.y = 0;
+        m_ThisTransform.forward = forwardNoY;
+        
+
+        m_ThisTransform.position += (forwardNoY * m_SpeedPerSecond * Time.deltaTime);
+    }
 
     //-------------------------------------------------------------------------
+
+    #endregion
+
+
+
+    #region public methods
+
+    #endregion
+
+
+
+    #region private methods
+
+
+
+    #endregion
 }
---
+++ Prototype_v0/Assets/_Scripts/EnemySpawner.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+    #region public variables
+
+    public float m_SpawnPeriod;
+    public float m_SpawnPeriodJitter;
+    public GameObject m_EnemyType; // THIS MUST BE A PREFAB!
+
+    #endregion
+
+
+
+    #region private variables
+
+    Vector3 m_SpawnerPosition = new Vector3();
+    private float m_StartTime;
+    private float m_ActualSpawnTime;
+
+    #endregion
+
+
+
+    #region unity overrides
+
+
+    // Use this for initialization
+    void Start()
+    {
+        m_SpawnerPosition = this.transform.position;
+        m_StartTime = 0;
+        m_ActualSpawnTime = m_SpawnPeriod + Random.RandomRange(-m_SpawnPeriodJitter, m_SpawnPeriodJitter);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_StartTime > m_ActualSpawnTime)
+        {
+            SpawnEnemy();
+            m_ActualSpawnTime = m_SpawnPeriod + Random.RandomRange(-m_SpawnPeriodJitter, m_SpawnPeriodJitter);
+            m_StartTime = 0;
+        }
+        else
+        {
+            m_StartTime += Time.deltaTime;
+        }
+    }
+
+
+    #endregion
+
+
+
+    #region public methods
+
+    #endregion
+
+
+
+    #region private methods
+
+    private void SpawnEnemy()
+    {
+        GameObject enemy = (GameObject)Instantiate(m_EnemyType);
+        enemy.transform.position = m_SpawnerPosition;
+    }
+
+    #endregion
+}
