98d11fc6bc57b67dc8f1c45ac0c2658467d8f3c0
timrighettini <tim.righettini@gmail.com>
2014-03-21 18:51:25 -0700

    Added in new player movement code.
    The light and shadow characters move if Split is set in gamemanager.
    The twilight character moves if Fused is set in gamemanager.

--- Prototype_v0/Assets/_Scripts/GameManager.cs
+++ Prototype_v0/Assets/_Scripts/GameManager.cs
@@ -6,21 +6,62 @@ public class GameManager : MonoBehaviour
 
     #region public variables
 
-    GameObject m_lightPlayer;
-    GameObject m_ShadowPlayer;
-    GameObject m_TwilightPlayer;
+    PlayerManager m_LightPlayer;
+    PlayerManager m_ShadowPlayer;
+    PlayerManager m_TwilightPlayer;
+
+    public enum playerState { SPLIT, FUSED };
+    public playerState e_PlayerState;
 
     #endregion
+
+
+
+    #region private variables
+
+
+
+    #endregion
+
+
+
+    #region unity overrides
 
     // Use this for initialization
 	void Start () {
 	    // Grab all of the player references, make it automated
-
+        m_LightPlayer = GameObject.FindGameObjectWithTag("LightPlayer").GetComponent<PlayerManager>();
+        m_ShadowPlayer = GameObject.FindGameObjectWithTag("ShadowPlayer").GetComponent<PlayerManager>();
+        m_TwilightPlayer = GameObject.FindGameObjectWithTag("TwilightPlayer").GetComponent<PlayerManager>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	    // Do Input Polling
+        if (e_PlayerState == playerState.SPLIT)
+        {
+            m_LightPlayer.Move(Input.GetAxis("P1_Horizontal"), Input.GetAxis("P1_Vertical"));
+            m_ShadowPlayer.Move(Input.GetAxis("P2_Horizontal"), Input.GetAxis("P2_Vertical"));
+            
+        }
+        else if (e_PlayerState == playerState.FUSED)
+        {
+            //TODO: Add fused movement and ability usage
+            m_TwilightPlayer.Move(Input.GetAxis("P1_Horizontal"), Input.GetAxis("P1_Vertical"));
+        }
+    }
 
-	}
+    #endregion 
+
+
+
+    #region public methods
+
+    #endregion
+
+
+
+    #region private methods
+
+    #endregion
 }
---
+++ Prototype_v0/Assets/_Scripts/PlayerManager.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerManager : MonoBehaviour {
+
+    #region public variables
+
+    public float m_movementSpeed;
+    public enum playerType { LIGHT, SHADOW, TWILIGHT };
+    public playerType e_PlayerType;
+
+    #endregion
+
+
+
+    #region private variables
+
+    private GameObject m_playerGameobject;
+            
+    #endregion
+
+
+
+    #region unity overrides
+
+    // Use this for initialization
+    void Start()
+    {
+        m_playerGameobject = this.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+
+    }
+
+    #endregion
+
+
+
+    #region public methods
+
+    public void Move(float dx, float dy)
+    {        
+        m_playerGameobject.transform.Translate(dx * m_movementSpeed * Time.deltaTime, 0, dy * m_movementSpeed * Time.deltaTime);
+    }
+
+    public void UseWeapon()
+    {
+        // Swing weapon if one is currently held
+    }
+
+    #endregion
+
+
+
+    #region private methods
+
+
+
+    #endregion
+}
