63bbcb62efca1bfc3bedc3620655b9eb32bc386c
timrighettini <tim.righettini@gmail.com>
2014-03-26 11:03:54 -0700

    Added in controller support and made the level a little better.

--- Prototype_v0/Assets/_Scripts/EnemySpawner.cs
+++ Prototype_v0/Assets/_Scripts/EnemySpawner.cs
@@ -12,6 +12,7 @@ public class EnemySpawner : MonoBehaviour {
     public Vector3 m_EnemyStartingDirection = new Vector3(0,0,-1);
     public float m_StartingMovementTime = 0.0f;
     public Vector3 m_StartingDirectionJitter = Vector3.zero;
+    public float m_EnemySpeedPerSecond = 5.0f;
 
     #endregion
 
@@ -69,13 +70,15 @@ public class EnemySpawner : MonoBehaviour {
     private void SpawnEnemy()
     {
         GameObject enemy = (GameObject)Instantiate(m_EnemyType);
+        BasicEnemyBehavior enemyBehavior = enemy.GetComponent<BasicEnemyBehavior>();
         enemy.transform.position = m_SpawnerPosition;
-        enemy.GetComponent<BasicEnemyBehavior>().m_DelayForStartingMovement = m_StartingMovementTime;
-        enemy.GetComponent<BasicEnemyBehavior>().m_StartingMovement = new Vector3(
+        enemyBehavior.m_DelayForStartingMovement = m_StartingMovementTime;
+        enemyBehavior.m_SpeedPerSecond = m_EnemySpeedPerSecond;
+        enemyBehavior.m_StartingMovement = new Vector3(
             m_EnemyStartingDirection.x + Random.Range(-m_StartingDirectionJitter.x, m_StartingDirectionJitter.x),
             m_EnemyStartingDirection.y + Random.Range(-m_StartingDirectionJitter.y, m_StartingDirectionJitter.y),
             m_EnemyStartingDirection.z + Random.Range(-m_StartingDirectionJitter.z, m_StartingDirectionJitter.z)
-            );
+        );
     }
 
     #endregion
